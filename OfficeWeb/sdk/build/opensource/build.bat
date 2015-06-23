dbf21ee32f9a54ae2e8511b13bc729ead9037e07
agolybev <agolybev@yandex.ru>
2015-06-23 12:47:45 +0300

    Deleted unused build scripts.

--- OfficeWeb/sdk/build/opensource/build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /D %~dp0
-
-@echo on
-call grunt.cmd --no-color
--- OfficeWeb/sdk/build/opensource/installtools.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /D %~dp0
-
-echo Installation grunt-cli
-call npm install -g grunt-cli
-
-call npm install
-
