f0ca21da8935f8ee6627cc75722685b1abc8b981
agolybev <agolybev@yandex.ru>
2015-08-18 14:05:31 +0300

    Removed unused code.

--- Tools/Bin/GenerateAllFonts.sh
+++ Tools/Bin/GenerateAllFonts.sh
@@ -3,12 +3,8 @@
 DIR="/var/www/onlyoffice/documentserver"
 
 #Start generate AllFonts.js, font thumbnails and font_selection.bin
-sudo -u onlyoffice "$DIR/Tools/AllFontsGen"\
+"$DIR/Tools/AllFontsGen"\
  "/usr/share/fonts"\
  "$DIR/DocService/OfficeWeb/sdk/Common/AllFonts.js"\
  "$DIR/DocService/OfficeWeb/sdk/Common/Images"\
  "$DIR/FileConverterService/Bin/font_selection.bin"
-
-#Restart web-site and converter
-sudo supervisorctl restart DocService
-sudo supervisorctl restart FileConverterService
